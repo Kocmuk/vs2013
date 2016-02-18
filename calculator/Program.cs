17358754377fc66bbe35fd06d74203c2f8f6a446
Konstantin Mahotov <cs@xcsm.ru>
2016-02-19 00:22:02 +0300

    test

---
+++ calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            calc.Add(10f, 20f);
+                
+        }
+    }
+
+    class Calculator
+    {
+        public float Add(float x, float y)
+        {
+            Console.WriteLine("Сумма {0} и {1} равна {2}", x, y, x + y);
+
+            Console.ReadLine();
+
+            Console.ReadLine();
+            return x + y;
+        }
+    }
+}
